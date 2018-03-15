d4a59f471fff521f7ca7605c3635b7b0c82d26e7
Adam Sheets <adamsheets89@gmail.com>
2018-03-15 11:06:09 -0500

    added htmlRoutes and controllers

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,27 @@
 
-
-
-
 //javascript function to take user inputs and update allies_db
 
+var index = function (app) {
+    app.get("/", function (req, res) {
+        res.render("index")
+    })
+};
+
+var allies = function (app) {
+    app.get("/allies", function (req, res) {
+        res.render("allies")
+    })
+};
+
+//this one will need to display the call buttons for the user to select 
+var sendHelp = function (app) {
+    app.get("/call", function (req, res) {
+        res.render("call", )
+    })
+};
+
+module.exports = {
+    index: index,
+    allies: allies,
+    sendHelp: sendHelp
+};
--- server.js
+++ server.js
@@ -11,6 +11,12 @@ var port = 4040;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//route controllers
+var htmlController = require("./app/routing/htmlRoutes.js");
+
+htmlController.index(app);
+htmlController.allies(app);
+htmlController.sendHelp(app);
 
 app.listen(port, function () {
     console.log("We are connected at " + port)
