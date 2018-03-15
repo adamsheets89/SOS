fd6807705673b6b42598e10b1a00d2088a25964c
Adam Sheets <adamsheets89@gmail.com>
2018-03-15 12:31:30 -0500

    added post route to update database

---
+++ app/routing/postRoutes.js
@@ -0,0 +1,22 @@
+var connection = require("../../config/connection")
+
+var addAlliesToDB = function (app) {
+    app.post("/allies", function (req, res) {
+        var allyName = req.body.name;
+        var allyPhone = req.body.phone;
+        var allyEmail = req.body.email;
+        connection.query("INSERT INTO allies SET ?",
+            {
+                ally_name: allyName,
+                phone_number: req.body.phone,
+                email_add: req.body.email,
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("Ally was successfully added to the database.")
+            }
+        );
+    })
+}
+
+module.exports = addAlliesToDB;
