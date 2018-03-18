cd7c37c9d9d7f3c748401c56e6c0b8af136f0e12
Adam Sheets <adamsheets89@gmail.com>
2018-03-18 15:23:53 -0500

    new user logic

--- app/routing/postRoutes.js
+++ app/routing/postRoutes.js
@@ -4,7 +4,7 @@ var connection = require("../config/connection")
 // need another route for pulling up all existing allies
 // app.get("/api/allies")
 var displayDB = function (app) {
-    app.get("/api/allies", function(req, res) {
+    app.get("/api/allies", function (req, res) {
         connection.query("SELECT * FROM allies", function (err, results) {
             for (var i = 0; i > results.length; i++) {
                 var allyName = results[i].ally_name;
@@ -32,7 +32,7 @@ var updateDB = function (app) {
                 phone_number: allyPhone,
                 email_add: allyEmail,
             },
-            
+
             function (err, data) {
                 console.log("line 35");
                 if (err) throw err;
@@ -45,23 +45,43 @@ var updateDB = function (app) {
 
 var newUser = function (app) {
     app.post("/api/users", function (req, res) {
-        var fullName = req.body.name;
+        var firstName = req.body.firstName;
+        var lastName = req.body.lastName;
         var username = req.body.username;
         var userEmail = req.body.email;
-        var userPhone =  req.body.phone;
+        var userPhone = req.body.phone;
         var userPass = req.body.password;
 
-        console.log("54 " + fullName);
+        console.log("54 " + firstName);
+        console.log("55 " + lastName);
         console.log("55 " + username);
         console.log("56 " + userEmail);
         console.log("57 " + userPhone);
-        console.log("58 " + userPass);
+        // console.log("58 " + userPass);
+
+        connection.query("INSERT INTO users SET ? ",
+            {
+                first_name: firstName,
+                last_name: lastName,
+                phone_number: userPhone,
+                email_add: userEmail,
+                user_name: username,
+                user_password: userPass
+            },
+
+            function (err, data) {
+                console.log("line 73");
+                if (err) throw err;
+                console.log("User was successfully added to the database.")
+                res.json(data)
+            }
+        )
     })
 
 }
 
-module.exports = { 
+module.exports = {
     updateDB: updateDB,
     displayDB: displayDB,
     newUser: newUser
- }
+}
