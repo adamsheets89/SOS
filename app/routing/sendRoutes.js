1de12af80366e5d7e8e113b91d3186696b317968
Adam Sheets <adamsheets89@gmail.com>
2018-03-20 14:38:17 -0500

    fixed table

--- app/routing/sendRoutes.js
+++ app/routing/sendRoutes.js
@@ -1,43 +1,47 @@
 var connection = require("../config/connection")
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config({ path: __dirname + '/.env' })
 
 const Nexmo = require('nexmo');
 
 
-   //this one will need to display the call buttons for the user to select 
+//this one will need to display the call buttons for the user to select 
 var sendMessage = function (app) {
   app.post("/text", function (req, res) {
-      // res.render("call")
-      console.log("this is the body" , req.body)
-      
-      const nexmo = new Nexmo({
-  apiKey: "430a0ec3",
-  apiSecret: "3Gp7OFe35mbC3y8D"
-});
+    // res.render("call")
+    console.log("this is the body", req.body)
 
-//function with for loop to run through allies and send a message to each phone number
-connection.query ('SELECT * FROM allies', function(err, thingsFromDb){
-  console.log("thingsFromDb", thingsFromDb)
+    const nexmo = new Nexmo({
+      apiKey: "430a0ec3",
+      apiSecret: "3Gp7OFe35mbC3y8D"
+    });
 
-nexmo.message.sendSms(
-    12092694056, thingsFromDb[i], req.body.message,
-      (err, responseData) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.dir(responseData);
-        }
+    //function with for loop to run through allies and send a message to each phone number
+    connection.query('SELECT * FROM allies WHERE user_id = ?', [req.session.user_id], function (err, thingsFromDb) {
+      console.log("thingsFromDb", thingsFromDb)
+
+      for (var i = 0; i > thingsFromDb.length; i++) {
+        var phoneNumber = thingsFromDb[i].phone_number;
+        console.log("phone: " + phoneNumber);
+
+        nexmo.message.sendSms(
+          12092694056, phoneNumber, req.body.message,
+          (err, responseData) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.dir(responseData);
+            }
+          }
+        );
       }
-   );
-});
+    });
 
 
-      
+
   })
 };
 
 
 module.exports = {
-message: sendMessage
+  message: sendMessage
 };
-   
